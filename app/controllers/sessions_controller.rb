996cc87bfeab6d6ce3cee3b8ec3e7bdbb9a27f94
jaspercurry <jaspercurry@gmail.com>
2016-10-03 16:40:11 -0400

    Add post request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 require 'rest-client'
+require 'httparty'
 
 class SessionsController < ApplicationController
   skip_before_action :authenticate
@@ -30,14 +31,35 @@ class SessionsController < ApplicationController
 
     final_url = url + client_id_formated + response_type + callback + userscope
     binding.pry
-    the_first_authorize = RestClient.get(final_url)
+    # the_first_authorize = RestClient.get(final_url)
 
   end
 
   def authcode
+    code= params["code"]
+    grant_type= "authorization_code"
+    redirect_uri="http%3A%2F%2Flocalhost%3A3000%2Fcallback%2F"
     binding.pry
+  token  = HTTParty.post("https://accounts.spotify.com/api/token",
+   body: {
+    client_id: ENV["client_id"],
+    client_secret: ENV["client_secret"],
+    redirect_uri:   redirect_uri,
+    grant_type: "authorization_code",
+    code: code
+    }
+    )
+  #  .to_json,
+    # headers: {'Content-Type' => 'application/json'}
+
+    token_code(token)
   end
 
+def token_code(token)
+artists =  HTTParty.get("https://api.spotify.com/v1/me/top/artists?limit=10",
+  headers: {Accept: "application/json", Authorization: "Bearer #{token["access_token"]}"})
+ binding.pry
+end
 
   # private
   #   def auth_params
